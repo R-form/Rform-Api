64a8e158d5ae892edf8ee978566d13b5e101010f
krystal <krystal93002232@gmail.com>
2022-04-26 23:35:26 +0800

    add issues#12 add dotenv

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,5 @@ Rails.application.routes.draw do
   resources :questionnaires do
     resources :responses
   end
-  get 'user_sessions/new'
-  get 'user_sessions/create'
-  get 'user_sessions/destroy'
-  root to: 'users#index'
-  resources :users
-  get 'login' => 'user_sessions#new', :as => :login
-  post 'login' => 'user_sessions#create'
-  delete 'logout' => 'user_sessions#destroy', :as => :logout
+  root "questionnaires#index"
 end
